aba9cdbaac56262a33d63ca262c0f38d3446f58d
mdhtr <mdhtr@users.noreply.github.com>
2018-06-09 13:33:59 +0200

    Code cleanup
    - Simpler GET endpoint implementation
    - Google Collections dependency added for convenience.

--- src/main/java/mdhtr/webapplication/HelloWorldApplication.java
+++ src/main/java/mdhtr/webapplication/HelloWorldApplication.java
@@ -1,18 +1,16 @@
 package mdhtr.webapplication;
 
+import com.google.common.collect.ImmutableSet;
 import mdhtr.webapplication.endpoint.HelloWorldEndpoint;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("")
 public class HelloWorldApplication extends Application {
     @Override
     public Set<Object> getSingletons() {
-        HashSet<Object> set = new HashSet<>();
-        set.add(new HelloWorldEndpoint());
-        return set;
+        return ImmutableSet.of(new HelloWorldEndpoint());
     }
 }
--- src/main/java/mdhtr/webapplication/endpoint/HelloWorldEndpoint.java
+++ src/main/java/mdhtr/webapplication/endpoint/HelloWorldEndpoint.java
@@ -2,14 +2,12 @@ package mdhtr.webapplication.endpoint;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 
 @Path("/")
 public class HelloWorldEndpoint {
     @GET
     @Path("/message")
-    public Response getMessage() {
-        String result = "Text from a REST API";
-        return Response.status(200).entity(result).build();
+    public String getMessage() {
+        return "Text from a REST API";
     }
 }
