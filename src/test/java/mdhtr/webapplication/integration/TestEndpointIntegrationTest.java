15aa870dac58de4d2bf2c5eab234017d6ec3f8fb
mdhtr <mdhtr@users.noreply.github.com>
2020-04-26 18:24:02 +0200

    #9 Example: modified parsing of incoming JSON object
    I've read that jackson-core and jackson-databind is a dependency of jackson-annotations, but it seems that this application works fine without those two, so I'm not adding them at this moment.

--- src/test/java/mdhtr/webapplication/integration/TestEndpointIntegrationTest.java
+++ src/test/java/mdhtr/webapplication/integration/TestEndpointIntegrationTest.java
@@ -13,6 +13,9 @@ class TestEndpointIntegrationTest {
     private static final int TEST_PORT = 9000;
     private static final String MESSAGE = "test message";
     private static final String JSON_MESSAGE_REQUEST_DTO = "{\"message\":\"" + MESSAGE + "\"}";
+    private static final String JSON_MESSAGE_RESPONSE_WITH_NO_DATE = "{\"message\":\"" + MESSAGE + "\",\"date\":null}";
+    private static final String JSON_MESSAGE_DTO_WITH_UNKNOWN_PROPERTIES = "{\"message\":\"" + MESSAGE + "\",\"extra-property\": \"extra\"}";
+    private static final String JSON_MESSAGE_DTO_WITH_DATE = "{\"message\":\"" + MESSAGE + "\",\"date\":\"2020-04-26 16:43:53\"}";
 
     private JettyServer server;
 
@@ -45,6 +48,33 @@ class TestEndpointIntegrationTest {
                 .post("/api/test/json")
                 .then()
                 .statusCode(200)
-                .body(is(JSON_MESSAGE_REQUEST_DTO));
+                .body(is(JSON_MESSAGE_RESPONSE_WITH_NO_DATE));
+    }
+
+    @Test
+    void serializeJson_shouldParseObjectWithUnknownProperties() {
+        given()
+                .port(TEST_PORT)
+                .contentType(ContentType.JSON)
+                .body(JSON_MESSAGE_DTO_WITH_UNKNOWN_PROPERTIES)
+                .when()
+                .post("/api/test/json")
+                .then()
+                .body(is(JSON_MESSAGE_RESPONSE_WITH_NO_DATE))
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    void serializeJson_shouldParseObjectWithDate() {
+        given()
+                .port(TEST_PORT)
+                .contentType(ContentType.JSON)
+                .body(JSON_MESSAGE_DTO_WITH_DATE)
+                .when()
+                .post("/api/test/json")
+                .then()
+                .statusCode(200)
+                .body(is(JSON_MESSAGE_DTO_WITH_DATE));
     }
 }
